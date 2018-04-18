33531af927d8ac2fcc0c34a9a41678181171ce1e
racosam <racosam@gmail.com>
2018-04-18 13:36:00 +0200

    dfgdsfg

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php include("inc/head.php");?>
 	<div id="content" class="container-fluid">
-		<div class="row">			
+		<div class="row">
+			<span>akjsdhfkj</span>			
 		</div>
 	</div>
 <?php include("inc/footer.php");?>
